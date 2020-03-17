594af37bd8517ecb42c56a2961e0bfee2079e52c
s13777 <s13777@pjwstk.edu.pl>
2020-03-17 14:18:23 +0100

    Final

--- cw3/Controllers/StudentsController.cs
+++ cw3/Controllers/StudentsController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using cw3.DAL;
+using cw3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cw3.Controllers
@@ -10,10 +13,67 @@ namespace cw3.Controllers
     [Route(template:"api/students")]
     public class StudentsController : ControllerBase
     {
-        [HttpGet(template:"{id}")]
-        public string GetStudents(int id)
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
         {
-            return "Kowalski, Malewski, Andrzejewski";
+            _dbService = dbService;
         }
+
+        [HttpGet]
+        public IActionResult GetStudent(string orderBy)
+        {
+            return Ok(_dbService.GetStudent());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            } else if (id == 2)
+            {
+                return Ok("Malewski");
+            } else if (id == 3)
+            {
+                return Ok("Andrzejewski");
+            }
+            return NotFound("Nie znaleziono studenta!");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //... add to database
+            //... generating index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            if (id == 1 || id == 2 || id == 3)
+            {
+                return Ok("Usuwanine ukończone.");
+
+            }
+            return NotFound("Nie ma takiego studenta.");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutStudent(int id)
+        {
+            if (id == 1 || id == 2 || id == 3)
+            {
+                return Ok("Aktualizacja dokończona.");
+
+            }
+            return NotFound("Nie ma takiego studenta.");
+        }
+
+
     }
 }
