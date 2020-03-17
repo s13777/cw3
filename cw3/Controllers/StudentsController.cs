660e726a1e8dca9dda4f7b5e5c7056926541844b
s13777 <s13777@pjwstk.edu.pl>
2020-03-17 11:26:36 +0100

    first

---
+++ cw3/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    [ApiController]
+    [Route(template:"api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet(template:"{id}")]
+        public string GetStudents(int id)
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+    }
+}
